f15af08ec9879ff30748a25012df801b626d5033
GiorgioDordo <djole6789@gmail.com>
2024-11-05 21:36:21 +0100

    changed controller and model

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,14 +1,19 @@
 
 @extends("layouts.app")
 
-@section("page-title", "Homepage")
+@section("page-title", "Projects")
 
 @section("content")
 <main class="container">
     <div class="row">
+        <h1>projects</h1>
+        @foreach ($projects as $index => $project)
         <div class="col-4 card">
-            <h1></h1>
+            <div class="card-body">
+                <p><strong>{{$project["name"]}}</strong></p>
+            </div>
         </div>
+        @endforeach
     </div>
 </main>
 @endsection
