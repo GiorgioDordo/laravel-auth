cedd5076a201f6a3944749b4b7da3a455ec45eb7
GiorgioDordo <djole6789@gmail.com>
2024-11-05 21:43:33 +0100

    printed projects on DOM

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,12 +5,15 @@
 
 @section("content")
 <main class="container">
-    <div class="row">
-        <h1>projects</h1>
+    <div class="row d-flex justify-content-center">
+        <div><h1 class="text-center">PROJECTS</h1></div>
         @foreach ($projects as $index => $project)
-        <div class="col-4 card">
+        <div class="col-3 m-3 p-3 card">
             <div class="card-body">
                 <p><strong>{{$project["name"]}}</strong></p>
+                <p>{{$project["languages"]}}</p>
+                <p>{{$project["short_description"]}}</p>
+                <p>{{$project["description"]}}</p>
             </div>
         </div>
         @endforeach
